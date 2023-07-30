bacaeb0dc86428c8acbd007041b438cbbd9f831d
Abingha2 <arbb211@gmail.com>
2023-07-30 00:58:45 -0500

    many gifs and sizing. modified API call

--- JS/app.js
+++ JS/app.js
@@ -2,12 +2,20 @@ async function ready() {
     let userMessage =prompt("Please enter Gify Api Key")
     localStorage.setItem("Api Key",userMessage)
     const gifyApi= userMessage
-    const result =await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${gifyApi}&tag=&rating=g`,{headers:{"Content-Type": "application/json"}})
+    const endpoint = "trending"
+    const limit = 16
+    const offset = 0
+    const rating = "pg-13"
+    const result =await fetch(`https://api.giphy.com/v1/gifs/${endpoint}?api_key=${gifyApi}&tag=&rating=${rating}&offset=${offset}&limit=${limit}`,{headers:{"Content-Type": "application/json"}})
     let test1=(await result.json())
-    console.log(test1.data.images.preview_gif.url)
+    console.log(test1.data[0].images.preview_gif.url)
     const display=document.querySelector(".gifmain")
-    const gif=document.createElement("img")
-    gif.src=test1.data.images.preview_gif.url
+    for (const image of test1.data){
+        const gif=document.createElement("img")
+    gif.src=image.images.preview_gif.url
+    gif.classList.add("bees")
     display.appendChild(gif)
+    }
+    
 }
 document.addEventListener("DOMContentLoaded",ready)
